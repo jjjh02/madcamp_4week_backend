1d5c2daaa55d09e3eb0c1e7483e96aea917cf79d
jjjh02 <jinnyshur@korea.ac.kr>
2024-01-21 14:51:26 +0900

    fix: cors 규칙 에러 수정

---
+++ week4/src/main/java/madcamp/week4/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package madcamp.week4;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // React 애플리케이션 URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
