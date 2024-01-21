0fa04221d23f7fc5b91ee356b5d921a17d16a976
Seunghyo <shyo0218@naver.com>
2024-01-21 15:05:05 +0900

    feat: group repository

--- week4/src/main/java/madcamp/week4/repository/OrganizationRepository.java
+++ week4/src/main/java/madcamp/week4/repository/OrganizationRepository.java
@@ -1,4 +1,10 @@
 package madcamp.week4.repository;
 
-public class OrganizationRepository {
+import madcamp.week4.model.Organization;
+import madcamp.week4.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {
 }
--- week4/src/main/java/madcamp/week4/service/OrganizationService.java
+++ week4/src/main/java/madcamp/week4/service/OrganizationService.java
@@ -1,4 +1,20 @@
 package madcamp.week4.service;
 
+import madcamp.week4.model.Organization;
+import madcamp.week4.repository.OrganizationRepository;
+import madcamp.week4.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrganizationService {
+
+    // 방생성 (초대코드 받아오기)
+    // 방들어가기
+    // 특정 그룹을 눌렀을때 해당하는 사람들의 리스트의 파일이 모두 떠야함
+
+    @Autowired
+    private OrganizationRepository organizationRepository;
+
+
 }
