daaf7b9057bd53f0f7c732b39f8f57c8a2084ae2
jjjh02 <jinnyshur@korea.ac.kr>
2025-04-07 18:00:09 +0900

    Mod: Organization 기능 수정

--- week4/src/main/java/madcamp/week4/model/Organization.java
+++ week4/src/main/java/madcamp/week4/model/Organization.java
@@ -1,15 +1,9 @@
 package madcamp.week4.model;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.*;
-import madcamp.week4.util.UniqueStringGenerator;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
-import java.util.Random;
 
 @Entity
 @Getter
@@ -23,15 +17,9 @@ public class Organization {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long organizationId;
 
-
     private String organizationName;
-
 
     private String organizationInviteNumber;
 
-    public void makeOrganization(String organizationName){
-        this.organizationName = organizationName;
-    }
-
-
+    public void changeOrganizationName(String newOrganizationName) { this.organizationName = newOrganizationName; }
 }
